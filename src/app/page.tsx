7852ceca30b2446cb28b935a1e4bc9b95d5a982a
Tyler <tyboyer16@gmail.com>
2025-04-11 23:39:11 -0500

    Removed CardTitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '@/components/ui/card'
 
 export default function Home() {
   return (
