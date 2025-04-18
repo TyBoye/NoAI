d2e21e8d17ef093acd1525ba275d6cad66dd1ff5
Md Istiaq Ahmed <mdistiaqahmed0@gmail.com>
2025-04-17 22:03:48 -0500

    fix

--- src/app/api/chat/utils/supabase.ts
+++ src/app/api/chat/utils/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient as supabaseCreateClient } from '@supabase/supabase-js';
 
-// Define your Supabase URL and public anon key from your Supabase project settings
+
 const SUPABASE_URL = process.env.SUPABASE_URL!;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
 
-// Create and export a Supabase client
 export const createClient = () => supabaseCreateClient(SUPABASE_URL, SUPABASE_ANON_KEY);
